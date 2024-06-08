bb347efceb852ff7d1615bf89b7532631bcbbbd9
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-07 23:52:58 -0300

    feat: add method with basic logic
    The method gets two strings, one from the user input and
    another one to compare if the digits match according
    to the game rules and, return a formatted string

--- src/main/java/com/bullsandcows/Main.java
+++ src/main/java/com/bullsandcows/Main.java
@@ -1,38 +1,43 @@
 package com.bullsandcows;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("""
-                The secret code is prepared: ****.
-                                
-                Turn 1. Answer:
-                1234
-                Grade: 1 cow.
-                                
-                Turn 2. Answer:
-                5678
-                Grade: 1 cow.
-                                
-                Turn 3. Answer:
-                9012
-                Grade: 1 bull and 1 cow.
-                                
-                Turn 4. Answer:
-                9087
-                Grade: 1 bull and 1 cow.
-                                
-                Turn 5. Answer:
-                1087
-                Grade: 1 cow.
-                                
-                Turn 6. Answer:
-                9205
-                Grade: 3 bulls.
-                                
-                Turn 7. Answer:
-                9305
-                Grade: 4 bulls.
-                Congrats! The secret code is 9305.""");
+        try (Scanner scanner = new Scanner(System.in)) {
+            String userGuess = scanner.nextLine();
+            String secretCode = "9305";
+            System.out.println(getCowsAndBulls(userGuess, secretCode));
+        }
+    }
+
+    protected static String getCowsAndBulls(String input, String secret) {
+
+        int bulls = 0;
+        int cows = 0;
+
+        for (int i = 0; i < 4; i++) {
+            if (input.charAt(i) == secret.charAt(i)) {
+                bulls++;
+            } else if (input.contains(String.valueOf(secret.charAt(i)))) {
+                cows++;
+            }
+        }
+
+        String noCows = "%d bull(s)".formatted(bulls);
+        String noBulls = "%d cow(s)".formatted(cows);
+        String bullsAndCows = "%d bull(s) and %d cow(s)".formatted(bulls, cows);
+        String grade = "Grade: %s. The secret code is %s.";
+
+        if (bulls == 0 && cows == 0) {
+            return String.format("Grade: None. The secret code is %s.", secret);
+        } else if (cows == 0 && bulls > 0) {
+            return String.format(grade, noCows, secret);
+        } else if (bulls == 0 && cows > 0) {
+            return String.format(grade, noBulls, secret);
+        } else {
+            return String.format(grade, bullsAndCows, secret);
+        }
     }
 }
