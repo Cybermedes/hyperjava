8e8635db832258942aecd482181722d0ba2b0fb2
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-07-02 15:53:35 -0300

    feat: add convert to decimal
    Add a method to convert from decimal to binary, octal or hexadecimal and for vice-versa in REPL mode

--- src/main/java/com/numberconverter/Main.java
+++ src/main/java/com/numberconverter/Main.java
@@ -5,17 +5,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        String userPrompt;
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter number in decimal system: ");
-            int decimal = scanner.nextInt();
-            System.out.print("Enter target base: ");
-            int radix = scanner.nextInt();
-            System.out.printf("Conversion result: %s%n", convertDecimal(decimal, radix));
+            while (true) {
+                System.out.print("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+                userPrompt = scanner.nextLine();
+                switch (userPrompt) {
+                    case "/exit" -> System.exit(0);
+                    case "/from" -> System.out.println(convertFromDecimalToBase(scanner));
+                    case "/to" -> System.out.println(convertFromBaseToDecimal(scanner));
+                    default -> System.out.println("unknown command");
+                }
+                scanner.nextLine();
+                System.out.println();
+            }
         }
     }
 
-    protected static String convertDecimal(int decimal, int radix) {
-        return Integer.toString(decimal, radix);
+    protected static String convertFromDecimalToBase(Scanner scanner) {
+        System.out.print("Enter a number in decimal system: ");
+        int decimal = scanner.nextInt();
+        System.out.print("Enter the target base: ");
+        int base = scanner.nextInt();
+        return "Conversion result: " + Integer.toString(decimal, base);
     }
+
+    protected static String convertFromBaseToDecimal(Scanner scanner) {
+        System.out.print("Enter source number: ");
+        String number = scanner.nextLine();
+        System.out.print("Enter source base: ");
+        int base = scanner.nextInt();
+        return "Conversion to decimal result: " + Integer.parseInt(number,base);
+    }
+
 }
