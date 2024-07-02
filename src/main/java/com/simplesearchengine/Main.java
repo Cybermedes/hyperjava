31cec144e9722c0cfc47b14915c64dff34520564
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-07-02 19:59:50 -0300

    feat: expand the search
    The program reads text lines from the standard input and processes single-word queries. The program output all lines that contain the string from the query.

--- src/main/java/com/simplesearchengine/Main.java
+++ src/main/java/com/simplesearchengine/Main.java
@@ -1,26 +1,60 @@
 package com.simplesearchengine;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
+
+    private static final List<String> SAVED_DATA = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<String> savedData = new ArrayList<>();
         try (Scanner scanner = new Scanner(System.in)) {
-            String samples = scanner.nextLine();
-            String dataToSearch = scanner.nextLine();
-            Collections.addAll(savedData, samples.split("\\s+"));
-            System.out.println(getIndex(savedData, dataToSearch));
+            getUserPrompts(scanner);
         }
     }
 
-    public static String getIndex(List<String> samples, String searchWord) {
-        if (samples.contains(searchWord)) {
-            return String.valueOf(samples.indexOf(searchWord) + 1);
-        } else {
-            return "Not found";
+    public static void getUserPrompts(Scanner scanner) {
+        System.out.println("Enter the number of people:");
+        int numberOfPeople = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter all people:");
+        for (int i = 0; i < numberOfPeople; i++) {
+            SAVED_DATA.add(scanner.nextLine());
         }
+
+        System.out.println();
+        System.out.println("Enter the number of search queries:");
+        int numberOfSearches = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 0; i < numberOfSearches; i++) {
+            System.out.println();
+            System.out.println("Enter data to search people:");
+            String dataToSearch = scanner.nextLine();
+
+            System.out.println(searchForData(dataToSearch));
+        }
+    }
+
+    public static String searchForData(String searchWord) {
+        StringBuilder sb = new StringBuilder();
+        Pattern pattern = Pattern.compile(searchWord, Pattern.CASE_INSENSITIVE);
+
+        for (String savedDatum : SAVED_DATA) {
+            Matcher matcher = pattern.matcher(savedDatum);
+            if (matcher.find()) {
+                if (sb.isEmpty()) {
+                    sb.append(System.lineSeparator()).append("Found people:");
+                }
+                sb.append(System.lineSeparator()).append(savedDatum);
+            }
+        }
+        if (sb.isEmpty()) {
+            sb.append("No matching people found.");
+        }
+        return sb.toString();
     }
 }
