a9e67323e39f2f5b54b4833bc26711f5eb8c7d74
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-07-03 17:14:51 -0300

    feat: add method to expand word counting
    The program can now count the number of words in a sentence (delimited by period, exclamation and question marks). And classify text as hard or easy based on the average number of words per sentence.

--- src/main/java/com/readabilityscore/Main.java
+++ src/main/java/com/readabilityscore/Main.java
@@ -1,14 +1,32 @@
 package com.readabilityscore;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             String text = scanner.nextLine();
-            String readability = text.length() <= 100 ? "EASY" : "HARD";
-            System.out.println(readability);
+            System.out.println(checkReadability(text));
         }
+    }
+
+    public static String checkReadability(String text) {
+
+        String[] sentences = text.split("([.!?])");
+        int words = 0;
+        int numberOfSentences = sentences.length;
+
+        Pattern pattern = Pattern.compile("\\w+");
+        for (String sentence : sentences) {
+            Matcher matcher = pattern.matcher(sentence);
+            while (matcher.find()) {
+                words++;
+            }
+        }
+
+        return (double) words/numberOfSentences <= 10 ? "EASY" : "HARD";
     }
 }
