9485a28b944be233f10533f7d6f177d0cc38cdfe
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-07-12 22:11:42 -0300

    feat: add the help argument
    The program now accepts a --help as argument. If only help is provided, the program will stop working and print a warning message, the second argument is the name of the project to display more information about it.

--- src/main/java/com/introduction/Intro.java
+++ src/main/java/com/introduction/Intro.java
@@ -1,20 +1,45 @@
 package com.introduction;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Intro {
 
     public static void main(String[] args) {
-        try (InputStream is = Intro.class.getClassLoader().getResourceAsStream("introduction.txt")) {
-
-            if (is != null) {
-                byte[] introTextInBytes = is.readAllBytes();
-                String text = new String(introTextInBytes);
-                System.out.println(text);
+        try {
+            if (args.length == 0) {
+                readIntroductionFile();
             } else {
-                System.out.println("the file introduction.txt is empty");
+                if (args[0].equals("--help")) {
+                    readProjectHelpFile(args[1]);
+                } else {
+                    System.out.printf("Unknown '%s' command.%n", args[0]);
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.format("missing 'project name' argument%n");
+        }
+    }
+
+    private static void readIntroductionFile() {
+        try (InputStream is = Intro.class.getClassLoader().getResourceAsStream("help/introduction.txt");
+             var br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(is)));
+             Stream<String> lines = br.lines()) {
+            lines.forEach(System.out::println);
+        } catch (IOException e) {
+            System.err.format("%s%n", e);
+        }
+    }
+
+    private static void readProjectHelpFile(String projectName) {
+        final String fileName = "help/" + projectName + ".txt";
+        try (InputStream is = Intro.class.getClassLoader().getResourceAsStream(fileName);
+             var br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(is)));
+             Stream<String> lines = br.lines()) {
+            lines.forEach(System.out::println);
+        } catch (NullPointerException e) {
+            System.err.format("couldn't locate the project with the name: '%s'%n", projectName);
         } catch (IOException e) {
             System.err.format("%s%n", e);
         }
